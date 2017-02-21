af6c69b2451e9c957f82a40f67d85db3ff62bef9
joaopedrovr91 <joaopedrovr91@gmail.com>
2017-02-21 11:58:53 -0300

    Acrescentando retorno da prancha

--- Assets/Scripts/PranchaMove.cs
+++ Assets/Scripts/PranchaMove.cs
@@ -5,13 +5,19 @@ using UnityEngine;
 public class PranchaMove : MonoBehaviour {
 
     public float velocidade;
-
+    public float limite;
+    public float retorno;
 	void Start () {
 		
 	}
 	
 	void Update () {
         Vector3 velocidadeVetorial = Vector3.left * velocidade;
-        transform.position = transform.position + velocidadeVetorial * Time.deltaTime;
+        transform.localPosition = transform.localPosition + velocidadeVetorial * Time.deltaTime;
+
+        if(transform.localPosition.x <= limite)
+        {
+            transform.localPosition = new Vector3(retorno, transform.localPosition.y, transform.localPosition.z);
+        }
 	}
 }
