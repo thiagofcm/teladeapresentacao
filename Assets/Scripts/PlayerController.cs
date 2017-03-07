fe4494a65aebb1a1ce7b4fcf7685f7e8a5dd0e79
joaopedrovr91 <joaopedrovr91@gmail.com>
2017-03-07 12:19:22 -0300

    comecando programar a animacao do player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float ForcaDoPulo = 10f;
+
+    private Animator anim;
+    private Rigidbody rb;
+    private bool pulando = false;
+
+	void Start () {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+    }
+	
+	void Update () {
+		if(Input.GetMouseButtonDown(0))
+        {
+            anim.Play("pulando");
+            rb.useGravity = true;
+            pulando = true;
+        }
+	}
+
+    private void FixedUpdate()
+    {
+        if (pulando)
+        {
+            pulando = false;
+            rb.velocity = Vector3.zero;
+            rb.AddForce(Vector3.up * ForcaDoPulo, ForceMode.Impulse); 
+        }
+    }
+
+}
